6c9ef142af07e30963d234003f549654301c9493
Abdullah <abdullahkhrief@gmail.com>
2023-05-31 22:54:32 +0300

    Linters errors

--- src/__tests__/Details.test.js
+++ src/__tests__/Details.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import Details from '../components/Details';
-import store from '../redux/store';
+import store from '../Redux/store';
 
 describe('Component render testing', () => {
   it('Line page renders correctly', () => {
