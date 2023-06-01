95a0eb73457272f8e46829b607efe546719f78fc
Abdullah <abdullahkhrief@gmail.com>
2023-06-01 15:47:14 +0300

    tests has been added and linters

--- src/__tests__/mainpage.test.js
+++ src/__tests__/mainpage.test.js
@@ -14,3 +14,53 @@ describe('Component render testing', () => {
     expect(asFragment()).toMatchSnapshot();
   });
 });
+
+describe('<Home />', () => {
+  it('Line page renders correctly', () => {
+    // eslint-disable-next-line
+    const { asFragment } = render(
+      <Provider store={store}>
+        <MainPage />
+      </Provider>,
+    );
+
+    const mockData = [
+      { country: 'Test 1', city: 'test1' },
+      { country: 'Test 2', city: 'test2' },
+      { country: 'Test 3', city: 'test3' },
+    ];
+    const searched = mockData[1].country;
+
+    expect(searched).toBe('Test 2');
+  });
+});
+it('Line page renders correctly', () => {
+  // eslint-disable-next-line
+  const { asFragment } = render(
+    <Provider store={store}>
+      <MainPage />
+    </Provider>,
+  );
+  const mockData = [
+    { country: 'Test 1', city: 'test1' },
+    { country: 'Test 2', city: 'test2' },
+    { country: 'Test 3', city: 'test3' },
+  ];
+  const searched = mockData[0].country;
+  expect(searched).toBe('Test 1');
+});
+it('Line page renders correctly', () => {
+  // eslint-disable-next-line
+  const { asFragment } = render(
+    <Provider store={store}>
+      <MainPage />
+    </Provider>,
+  );
+  const mockData = [
+    { country: 'Test 1', city: 'test1' },
+    { country: 'Test 2', city: 'test2' },
+    { country: 'Test 3', city: 'test3' },
+  ];
+  const searched = mockData[2].city;
+  expect(searched).toBe('test3');
+});
